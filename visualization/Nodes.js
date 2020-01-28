337c28c72cb5f7a68c45c2e36c9918ab95f36771
RosbergO <36854121+RosbergO@users.noreply.github.com>
2020-01-28 13:43:29 +0100

    Add files via upload

---
+++ visualization/Nodes.js
@@ -0,0 +1,32 @@
+/*
+let nodes = [
+    {
+        "id": 1,
+        "name": "A"
+    },
+    {
+        "id": 2,
+        "name": "B"
+    },
+    {
+        "id": 3,
+        "name": "C"
+    },
+    {
+        "id": 4,
+        "name": "D"
+    },
+    {
+        "id": 5,
+        "name": "E"
+    }
+]
+
+let links = [
+    {
+        "source": 1,
+        "target": 2
+    }
+]
+
+ */
